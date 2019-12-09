74dad7453932aa55dc82ae7d7e65427f42dad95d
Rud Aleksey <alexey.rud@infonetsoft.ru>
2019-12-09 23:07:11 +0700

    update web. remove material ui.Update login response and add get user current info by authorized

--- web/src/common/layout/AppHeader.jsx
+++ web/src/common/layout/AppHeader.jsx
@@ -1,33 +1,32 @@
-import React, { Component } from 'react';
-import {
-    Link,
-    withRouter
-} from 'react-router-dom';
+import React, {Component} from 'react';
+import {Link, withRouter} from 'react-router-dom';
 import './AppHeader.css';
-import { Layout, Menu, Dropdown, Icon } from 'antd';
+import {Dropdown, Icon, Input, Layout, Menu} from 'antd';
 import PropTypes from 'prop-types'
 import FullScreen from "./FullScreen";
 
+const {Search} = Input;
 const Header = Layout.Header;
+
 class AppHeader extends Component {
     constructor(props) {
         super(props);
         this.handleMenuClick = this.handleMenuClick.bind(this);
     }
 
-    handleMenuClick({ key }) {
-        if(key === "logout") {
+    handleMenuClick({key}) {
+        if (key === "logout") {
             this.props.onLogout();
         }
     }
 
     render() {
         let menuItems;
-        if(this.props.currentUser) {
+        if (this.props.currentUser) {
             menuItems = [
                 <Menu.Item key="/">
                     <Link to="/">
-                        <Icon type="home" className="nav-icon" />
+                        <Icon type="home" className="nav-icon"/>
                     </Link>
                 </Menu.Item>,
                 <Menu.Item key="/home" className="profile-menu">
@@ -46,21 +45,23 @@ class AppHeader extends Component {
 
         return (
             <Header id='header' className="app-header">
-                    <div className="app-title" >
-                        <span>Cloud File Server</span>
-                    </div>
-
-
-                <div className="pull-right">
-                    <div className="app-menu">
-                        <Menu
+                <div className="app-title">
+                    <span>Cloud File Server</span>
+                </div>
+           {/*     <div className="app-menu">
+                    <Menu
                         mode="horizontal"
                         selectedKeys={[this.props.location.pathname]}
                     >
                         {menuItems}
                     </Menu>
-                    </div>
-                    <FullScreen className="pull-right" />
+                </div>*/}
+
+                <div className="pull-right">
+                    <div className="header-search pull-right"><Search placeholder="input search text"
+                                                                      onSearch={value => console.log(value)}/></div>
+
+                <FullScreen className="pull-right"/>
                 </div>
             </Header>
         );
@@ -78,7 +79,7 @@ function ProfileDropdownMenu(props) {
                     @{props.currentUser.firstName} {props.currentUser.lastName}
                 </div>
             </Menu.Item>
-            <Menu.Divider />
+            <Menu.Divider/>
             <Menu.Item key="profile" className="dropdown-item">
                 <Link to={`/users/${props.currentUser.login}`}>Profile</Link>
             </Menu.Item>
@@ -92,9 +93,9 @@ function ProfileDropdownMenu(props) {
         <Dropdown
             overlay={dropdownMenu}
             trigger={['click']}
-            getPopupContainer = { () => document.getElementsByClassName('profile-menu')[0]}>
+            getPopupContainer={() => document.getElementsByClassName('profile-menu')[0]}>
             <a className="ant-dropdown-link" href={"#detail"}>
-                <Icon type="user" className="nav-icon" style={{marginRight: 0}} /> <Icon type="down" />
+                <Icon type="user" className="nav-icon" style={{marginRight: 0}}/> <Icon type="down"/>
             </a>
         </Dropdown>
     );
@@ -109,14 +110,14 @@ AppHeader.propTypes = {
         firstName: PropTypes.string,
         lastName: PropTypes.string,
         langKey: PropTypes.string,
-        email:PropTypes.string,
+        email: PropTypes.string,
         activated: PropTypes.bool,
         createdBy: PropTypes.string,
         createdDate: PropTypes.instanceOf(Date),
-        imageUrl:PropTypes.string,
+        imageUrl: PropTypes.string,
         lastModifiedBy: PropTypes.string,
         lastModifiedDate: PropTypes.instanceOf(Date),
-        authorities:PropTypes.arrayOf(PropTypes.string)
+        authorities: PropTypes.arrayOf(PropTypes.string)
     }),
 };
 ProfileDropdownMenu.propTypes = {
